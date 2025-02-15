620f1bdb424b903b56fc6dd666663e90ddfef7c4
dinis <dinis@debian.dinis>
2025-02-15 15:28:07 -0300

    Merge feature/confirmacao-email-cadastro into user: implementação da funçao que envia email

--- src/main/java/com/dinis/cto/service/UserService.java
+++ src/main/java/com/dinis/cto/service/UserService.java
@@ -20,7 +20,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
